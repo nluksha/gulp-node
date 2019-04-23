f4db54978ddd007b738e4b77aabfb3fee149f364
Mikita Luksha <nikita.luksha@gmail.com>
2019-04-23 20:43:05 +0300

    Refactor

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,22 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 
-gulp.task('default', () => {
+
+function build() {
   return gulp.src('app/**.js', { sourcemaps: true })
     .pipe(babel({
       presets: ['@babel/preset-env', '@babel/preset-react']
     }))
     .pipe(concat('all.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist', { sourcemaps: true }));
-});
+    .pipe(gulp.dest('dist', { sourcemaps: true })); 
+}
 
-gulp.watch('app/**.js', cd => {
-  console.log('Changed');
+function rebuild() {
+  console.log('Files changed');
 
-  cd();
-})
+  return build();
+}
+
+gulp.task('default', build);
+gulp.watch('app/**.js', rebuild);
